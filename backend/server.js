89290f5ca2c883098d3a4e98fd73289bb171f3e8
PierreHorsmans <pierre.horsmans@gmail.com>
2025-06-28 14:10:33 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 console.log("ENV → PORT variable equals:", process.env.PORT);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 console.log(`Server läuft auf Port ${PORT}`);
 
 app.use(cors());
