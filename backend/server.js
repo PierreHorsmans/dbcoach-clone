20dd2b52bece5e10cd2fd352cc15693273ed0f12
PierreHorsmans <pierre.horsmans@gmail.com>
2025-06-28 13:11:27 +0200

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/api/anmelden", (req, res) => {
+  const { name, email } = req.body;
+
+  console.log("Empfangene Daten:", name, email);
+
+  res.status(200).json({ message: "Erfolgreich empfangen" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
