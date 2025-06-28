49548b06cff1b27471f71d40152ab54f1093ce24
PierreHorsmans <pierre.horsmans@gmail.com>
2025-06-28 18:12:13 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,10 @@
-const mysql = require("mysql2");
+const { Pool } = require('pg');
 
-const connection = mysql.createConnection({
-  host: "sql112.infinityfree.com",
-  user: "if0_39345328",
-  password: "LomOyzKZyE",
-  database: "if0_39345328_dbcoach"
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error("Verbindungsfehler:", err);
-  } else {
-    console.log("Verbindung erfolgreich hergestellt.");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Für Railway
   }
 });
 
-module.exports = connection;
+module.exports = pool;
